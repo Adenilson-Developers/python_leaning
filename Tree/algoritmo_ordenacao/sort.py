cad3f679360698ff925fc338e7f4fe425377d359
Adenilson-Developers <adenilson.developers@gmail.com>
2023-02-06 16:56:15 -0300

    add inseption sort

--- Tree/algoritmo_ordenacao/sort.py
+++ Tree/algoritmo_ordenacao/sort.py
@@ -1,4 +1,15 @@
 
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > chave:
+            lista[j+1] = lista[j]
+            j = j - 1
+        lista[j+1] = chave
+
+
 def dubble_sort(lista):
     n = len(lista)
     for j in range(n-1):
