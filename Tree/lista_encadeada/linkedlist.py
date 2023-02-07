d6ebf2b7058be3beeaa8aa804c0a667654f94cff
Adenilson-Developers <adenilson.developers@gmail.com>
2023-02-06 22:14:31 -0300

    implementando a função len

--- Tree/lista_encadeada/linkedlist.py
+++ Tree/lista_encadeada/linkedlist.py
@@ -5,7 +5,7 @@ from node import Node
 class LinkedList: 
     def __init__(self):
         self.head = None
-        self.size = 0
+        self._size = 0
     def append(self, elem):
         if self.head:
             # inserção quando a lista jpa possui elementos
@@ -16,7 +16,45 @@ class LinkedList:
         else:
             #primeira inserção
             self.head = Node(elem)
-        self.size = self.size + 1
+        self._size = self._size + 1
+    def __len__(self):
+        """ Retorna o tamanho da lista"""
+        return self._size
+    
+    def get(self, index):
+        pass
+    
+    def set(self, index, elem):
+        pass
+    
+    def __getitem__(self, index):
+        # a = lista[6]
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else: 
+                raise IndexError("list index out of range")
+
+        if pointer:
+            return pointer.data
+        else:
+            raise IndexError("list index out of range")
+
+    def __setitem__(self, index, elem):
+        # lista[5] = 9
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else: 
+                raise IndexError("list index out of range")
+
+        if pointer:
+            pointer.data = elem
+        else:
+            raise IndexError("list index out of range")
+    
         
 
 
