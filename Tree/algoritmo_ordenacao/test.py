9aa28fc98edd5e223193dea332c3c5ac1a249aa6
Adenilson-Developers <adenilson.developers@gmail.com>
2023-02-21 12:37:28 -0300

    automatizando teste

--- Tree/algoritmo_ordenacao/test.py
+++ Tree/algoritmo_ordenacao/test.py
@@ -12,11 +12,19 @@ repeated = [7,7,7,7,7,1,1,9,9,2,4,4,4,5,4,5,7,1]
 number = [3, 20, 6, 4, 50, 55, 1, 8, 20, 11]
 
 if __name__ == "__main__":
-    lista = number
-    print(lista)
-    print("\n Ordenado:")
-    #insertion_sort(lista)
-    quicksort(lista)
+        test_class = {
+            'Números eleatórios': any_numbers,
+            'Já ordenados': already_sort,
+            'Ordem Inversa': inversed,
+            'Elementos repetidos': repeated,
+            'Listando number': number
+        }
 
-    merge_sort(lista)
-    print(lista)
+        print("********************************")
+        for name, lista in test_class.items():
+            print("\n Caso de teste: {}".format(name))
+            print(lista)
+            merge_sort(lista)
+            print("\n Ordenado:")
+            print(lista)
+
