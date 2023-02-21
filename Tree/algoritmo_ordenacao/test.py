8161d6de0915d1198a58003b23df834c2a7a697c
Adenilson-Developers <adenilson.developers@gmail.com>
2023-02-21 12:34:23 -0300

    implementando quicksort

--- Tree/algoritmo_ordenacao/test.py
+++ Tree/algoritmo_ordenacao/test.py
@@ -1,9 +1,9 @@
 import random 
-from sort import select_sort, dubble_sort, insertion_sort, merge_sort
+from sort import select_sort, dubble_sort, insertion_sort, merge_sort, quicksort
 
 any_numbers = random.sample(range(1,1000), 42)
 
-already_short = [1,2,3,4,5,6,9,20,22,23,28,32,34,39,40,42,76,87,99,112]
+already_sort = [1,2,3,4,5,6,9,20,22,23,28,32,34,39,40,42,76,87,99,112]
 
 inversed = [117,90,88,83,81,77,74,69,64,63,51,50,49,42,41,34,32,29,28,22,16,8,6,5,3,1]
 
@@ -16,5 +16,7 @@ if __name__ == "__main__":
     print(lista)
     print("\n Ordenado:")
     #insertion_sort(lista)
+    quicksort(lista)
+
     merge_sort(lista)
     print(lista)
